3cb18f59c03c015c3a0b4a06d74bd2f865887a6a
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2024-02-13 10:28:30 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/Nivaldeir/golang-opportunities/router"
+import (
+	"github.com/Nivaldeir/golang-opportunities/config"
+	"github.com/Nivaldeir/golang-opportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config initilization error: %v", err)
+		return
+	}
 	router.Initialize()
 }

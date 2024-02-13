489fea8308ac00f9b6bb53ec7c83a633ad3b7422
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2024-02-13 00:11:24 -0300

    feat: update handler structure to separete packege

---
+++ handler/handler.go
@@ -0,0 +1,23 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{})
+}
+func ShowOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{})
+}
+func DeleteOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{})
+}
+func UpdateOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{})
+}
+func ListOpeningHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{})
+}
